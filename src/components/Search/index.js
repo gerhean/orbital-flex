58b150bec8fafd4187e713203f28cbbd0cb62f8c
Ger Hean <gerhean@live.com>
2019-07-06 21:44:51 +0800

    rename schedulelist

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from 'react-native';
-import ScheduleList from './ScheduleList';
+import SearchSchedules from './SearchSchedules';
 
 export default class Search extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScheduleList />
+        <SearchSchedules />
       </View>
     );
   }
