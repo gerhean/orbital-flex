cbc37103c040d91caa98cf074bc19b6748795e0b
Ger Hean <gerhean@live.com>
2019-07-19 14:47:49 +0800

    nicer looking pt 1

--- src/reducers/state.js
+++ src/reducers/state.js
@@ -20,45 +20,45 @@ export default initialState = () => ({
   },
 });
 
-const mockUser = {
-  timeFetched: 100000,
-  username: "test",
-  contact: "",
-  about: "I'm just a random person",
-  profilePic: "https://i.stack.imgur.com/l60Hf.png",
-  gender: 0,
-  bookedSchedules: {},
-  postedSchedules: {},
-  avgRating: 0,
-  numRatings: 0,
-  reviews: [],
-  ownReview: false,
-  timeFetchedReview: 100000,
-};
+// const mockUser = {
+//   timeFetched: 100000,
+//   username: "test",
+//   contact: "",
+//   about: "I'm just a random person",
+//   profilePic: "https://i.stack.imgur.com/l60Hf.png",
+//   gender: 0,
+//   bookedSchedules: {},
+//   postedSchedules: {},
+//   avgRating: 0,
+//   numRatings: 0,
+//   reviews: [],
+//   ownReview: false,
+//   timeFetchedReview: 100000,
+// };
 
-const scheduleExample = {
-	id: 1, // id in firestore database
-	poster: "some uid",
-  posterName: "some person",
-	bookers: {},
-  image: '',
-	location: '',
-	day: 0,
-  timeStart: 0,
-  timeEnd: 0,
-	price: '',
-	services: '',
-	remarks: '',
-  timeCreated: '',
-  isBooked: 0, // -1 means posted, 0 means not booked, 1 means booked
-}
+// const scheduleExample = {
+// 	id: 1, // id in firestore database
+// 	poster: "some uid",
+//   posterName: "some person",
+// 	bookers: {},
+//   image: '',
+// 	location: '',
+// 	day: 0,
+//   timeStart: 0,
+//   timeEnd: 0,
+// 	price: '',
+// 	services: '',
+// 	remarks: '',
+//   timeCreated: '',
+//   isBooked: 0, // -1 means posted, 0 means not booked, 1 means booked
+// }
 
-const publicUserInfo = {
-	uid: 'id',
-	name: 'somebody',
-	profilePic: 'image.com/image',
-	contact: '',
-}
+// const publicUserInfo = {
+// 	uid: 'id',
+// 	name: 'somebody',
+// 	profilePic: 'image.com/image',
+// 	contact: '',
+// }
 
 // const chatroomExample = {
 //   otherUid, 
