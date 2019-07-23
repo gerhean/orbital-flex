5daf44a382cb80349dedf3d8619a6ff31671f615
Ger Hean <gerhean@live.com>
2019-07-24 02:00:37 +0800

    Update store.js
    removed comments

--- src/store.js
+++ src/store.js
@@ -1,13 +1,9 @@
-// remember when I said this is just a standard store
-// this one is a little more advanced to show you
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-// this pulls in your combinedReducers
-// nav_reducer is one of them
 import reducers, { initialState } from "./reducers";
 import mainSaga from "./sagas";
 
@@ -26,13 +22,8 @@ const store = createStore(
   pReducer,
   initialState(),
   applyMiddleware(sagaMiddleware)
-  // autoRehydrate()
-  // compose(
-  //     applyMiddleware(thunk),
-  // )
 );
 
-// persistStore(store, { storage: AsyncStorage, whitelist: [] })
 sagaMiddleware.run(mainSaga);
 
 // this exports it for App.js
